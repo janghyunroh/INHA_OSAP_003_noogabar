c8beb408f2b52ecbed36b3669e830747e503bbf9
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2023-11-19 13:46:19 +0900

    create size and rank

--- Set_SizeRank.cpp
+++ Set_SizeRank.cpp
@@ -1,10 +1,29 @@
 #include "Set.h"
 
-//이정현
-int Set::Size() {
+int Set::Size() { return size_; }
 
+// 노드의 depth와 rank를 pair로 return하는 함수
+std::pair<int, int> Set::Rank(int arg) {
+  Node *node = Search(arg);
+  // tree에 노드가 없다면 0 return
+  if (node == nullptr)
+    return {0, 0};
+  int depth = Height(root_) - Height(node);
+  // int depth = Depth(arg);
+  int rank = Rank(root_, arg) + 1;
+  return {depth, rank};
 }
 
-std::pair<int, int> Set::Rank(int arg) {
-
+// rank를 return하는 함수
+// rank는 x보다 작은 수로 정의
+int Rank(Node *node, int x) {
+  if (!node)
+    return 0;
+  int cnt = 0;
+  if (node->data < x) {
+    cnt++;
+  }
+  cnt += Rank(node->left, x);
+  cnt += Rank(node->right, x);
+  return cnt;
 }
---
+++ test_size_rank.cc
@@ -0,0 +1,9 @@
+#include "AVL.h"
+#include <gtest/gtest.h>
+#include <iostream>
+
+TEST(AVLSizeTest, ZeroAndNegative){
+    Set::
+
+    EXPECT_EQ(0 ,size);
+}
