cb4a361b2c22d7f4170fd59fed3f06c05741dadb
LeeJeongHyun <76839646+jeonghyun06@users.noreply.github.com>
2023-11-20 21:48:59 +0900

    Create Size and Rank function and test code for size and rank

--- Set_SizeRank.cpp
+++ Set_SizeRank.cpp
@@ -3,27 +3,27 @@
 int Set::Size() { return size_; }
 
 // 노드의 depth와 rank를 pair로 return하는 함수
-std::pair<int, int> Set::Rank(int arg) {
+int Set::Rank(int arg) {
   Node *node = Search(arg);
   // tree에 노드가 없다면 0 return
   if (node == nullptr)
     return {0, 0};
+  int rank = CalculateRank(root_, arg) + 1;
   int depth = Height(root_) - Height(node);
   // int depth = Depth(arg);
-  int rank = Rank(root_, arg) + 1;
   return {depth, rank};
 }
 
 // rank를 return하는 함수
 // rank는 x보다 작은 수로 정의
-int Rank(Node *node, int x) {
+int CalculateRank(Node *node, int x) {
   if (!node)
     return 0;
   int cnt = 0;
   if (node->data < x) {
     cnt++;
   }
-  cnt += Rank(node->left, x);
-  cnt += Rank(node->right, x);
+  cnt += CalculateRank(node->left, x);
+  cnt += CalculateRank(node->right, x);
   return cnt;
 }
