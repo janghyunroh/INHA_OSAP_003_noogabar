114cb89f34be915b42c720e896d8da66f36bc5b0
JihoonRyu <harryryujh@naver.com>
2023-11-27 08:32:45 +0900

    Node클래스의 일부 함수들의 return type 수정

--- src/Node/node.cpp
+++ src/Node/node.cpp
@@ -2,15 +2,15 @@
 
 template <typename T1, typename T2> T1 getkey() { return this->key_; }
 
-template <typename T1, typename T2> T1 getdata() { return this->data_; }
+template <typename T1, typename T2> T2 getdata() { return this->data_; }
 
-template <typename T1, typename T2> T1 getleft() { return this->left_; }
+template <typename T1, typename T2> Node<T1, T2> *getleft() { return this->left_; }
 
 template <typename T1, typename T2> void setleft(Node<T1, T2> *node) {
   this->left = node;
 }
 
-template <typename T1, typename T2> T1 getright() { return this->right_; }
+template <typename T1, typename T2> Node<T1, T2> *getright() { return this->right_; }
 
 template <typename T1, typename T2> void setright(Node<T1, T2> *node) {
   this->right = node;
