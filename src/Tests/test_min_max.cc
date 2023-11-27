236400d82e26c7125b3060ab5461dcec03880467
ryujihoon00 <harryryujh@naver.com>
2023-11-28 03:33:45 +0900

    tree에 없는 노드를 입력받는 케이스에 대한 테스트 추가

--- src/Tests/test_min_max.cc
+++ src/Tests/test_min_max.cc
@@ -62,13 +62,15 @@ void MinMaxTestFixture::TearDown() {
  *
  */
 TEST_F(MinMaxTestFixture, TestMin) {
-  // key가 5, 3 인 노드가 root인 서브 트리의 leftmost 노드의 key 값의 기대값은 각각 4, 1
-  // key가 1, 9, 4인 노드는 leaf 노드이므로 기댓값은 자기자신의 key 값
+  // key가 5, 3 인 노드가 root인 서브 트리의 leftmost 노드의 key 값의 기대값은
+  // 각각 4, 1 key가 1, 9, 4인 노드는 leaf 노드이므로 기댓값은 자기자신의 key 값
   EXPECT_EQ(1, set_.Minimum(1));
   EXPECT_EQ(4, set_.Minimum(5));
   EXPECT_EQ(1, set_.Minimum(3));
   EXPECT_EQ(9, set_.Minimum(9));
   EXPECT_EQ(4, set_.Minimum(4));
+  // tree에 없는 노드를 입력받을 때 프로그램이 종료되기를 기대
+  EXPECT_DEATH(set_.Minimum(10), "");
 }
 
 /**
@@ -76,13 +78,15 @@ TEST_F(MinMaxTestFixture, TestMin) {
  *
  */
 TEST_F(MinMaxTestFixture, TestMax) {
-  // key가 5, 3 인 노드가 root인 서브 트리의 rightmost 노드의 key 값의 기대값은 9
-  // key가 1, 9, 4인 노드는 leaf 노드이므로 기댓값은 자기자신의 key 값
+  // key가 5, 3 인 노드가 root인 서브 트리의 rightmost 노드의 key 값의 기대값은
+  // 9 key가 1, 9, 4인 노드는 leaf 노드이므로 기댓값은 자기자신의 key 값
   EXPECT_EQ(1, set_.Maximum(1));
   EXPECT_EQ(9, set_.Maximum(5));
   EXPECT_EQ(9, set_.Maximum(3));
   EXPECT_EQ(9, set_.Maximum(9));
   EXPECT_EQ(4, set_.Maximum(4));
+  // tree에 없는 노드를 입력받을 때 프로그램이 종료되기를 기대
+  EXPECT_DEATH(set_.Maximum(10), "");
 }
 
 int main(int argc, char **argv) {
