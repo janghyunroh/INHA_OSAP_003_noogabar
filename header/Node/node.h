eb6a3624d2ada205d45c36185f8ae2a0fbddf068
ryujihoon00 <harryryujh@naver.com>
2023-12-11 15:09:58 +0900

    main.cpp, node.h 코드 수정

--- header/Node/node.h
+++ header/Node/node.h
@@ -15,15 +15,15 @@ template <typename T> class Node {
 public:
   Node(T key) : key_(key), left_(NULL), right_(NULL) {}
   T get_key() { return key_; }
-  Node *get_left() { return left_; }
-  void set_left(Node *node) { left_ = node; }
-  Node *get_right() { return right_; }
-  void set_right(Node *node) { right_ = node; }
+  Node<T> *get_left() { return left_; }
+  void set_left(Node<T> *node) { left_ = node; }
+  Node<T> *get_right() { return right_; }
+  void set_right(Node<T> *node) { right_ = node; }
 
 private:
   T key_;
-  Node *left_;
-  Node *right_;
+  Node<T> *left_;
+  Node<T> *right_;
 };
 
 #endif
